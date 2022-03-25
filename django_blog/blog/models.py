8366853651c6ab787e545d5b06a0d04bcd884ac2
Outhan Chazima <ochazima@jambopay.com>
2022-03-25 14:38:00 +0300

    fixed views

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 
@@ -31,3 +32,9 @@ class Post(models.Model):
 
     objects = models.Manager()  # The default manager.
     published = PublishedManager() # Our custom manager
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', 
+                        args=[self.publish.year,
+                                self.publish.month,
+                                self.publish.day, self.slug])
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,4 +3,12 @@ from .models import Post
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
+
+    return render(request, 'post/detail.html', {'post': post})
+
+
